4bde68ac767b262aae2b358ac950360ac4211203
Lucas Otanez <lotanez@usc.edu>
2024-01-12 15:39:09 -0800

    finish mapping piano keys

--- app/Piano.tsx
+++ app/Piano.tsx
@@ -66,61 +66,61 @@ export default function Piano() {
       <div id='b3' onMouseDown={(e) => note(e, 246.9, 'pure')} className="key"></div>
 
       {/* Octave 4 */}
-      <div id='c4' className="key">
-        <div id='c4#' className="sharp-key"></div>
+      <div id='c4' onMouseDown={(e) => note(e, 261.6, 'pure')} className="key">
+        <div id='c4#' onMouseDown={(e) => note(e, 277.1, 'sharp')} className="sharp-key"></div>
       </div>
-      <div id='d4' className="key">
-        <div id='d4#' className="sharp-key"></div>
+      <div id='d4' onMouseDown={(e) => note(e, 293.7, 'pure')} className="key">
+        <div id='d4#' onMouseDown={(e) => note(e, 311.1, 'sharp')} className="sharp-key"></div>
       </div>
-      <div id='e4' className="key"></div>
-      <div id='f4' className="key">
-        <div id='f4#' className="sharp-key"></div>
+      <div id='e4' onMouseDown={(e) => note(e, 329.6, 'pure')} className="key"></div>
+      <div id='f4' onMouseDown={(e) => note(e, 349.2, 'pure')} className="key">
+        <div id='f4#' onMouseDown={(e) => note(e, 369.99, 'sharp')} className="sharp-key"></div>
       </div>
-      <div id='g4' className="key">
-        <div id='g4#' className="sharp-key"></div>
+      <div id='g4' onMouseDown={(e) => note(e, 392, 'pure')} className="key">
+        <div id='g4#' onMouseDown={(e) => note(e, 415.3, 'sharp')} className="sharp-key"></div>
       </div>
-      <div id='a4' className="key">
-        <div id='a4#' className="sharp-key"></div>
+      <div id='a4' onMouseDown={(e) => note(e, 440, 'pure')} className="key">
+        <div id='a4#' onMouseDown={(e) => note(e, 466.16, 'sharp')} className="sharp-key"></div>
       </div>
-      <div id='b4' className="key"></div>
+      <div id='b4' onMouseDown={(e) => note(e, 493.88, 'pure')} className="key"></div>
 
       {/* Octave 5 */}
-      <div id='c5' className="key">
-        <div id='c5#' className="sharp-key"></div>
+      <div id='c5' onMouseDown={(e) => note(e, 523.25, 'pure')} className="key">
+        <div id='c5#' onMouseDown={(e) => note(e, 554.37, 'sharp')} className="sharp-key"></div>
       </div>
-      <div id='d5' className="key">
-        <div id='d5#' className="sharp-key"></div>
+      <div id='d5' onMouseDown={(e) => note(e, 587.33, 'pure')} className="key">
+        <div id='d5#' onMouseDown={(e) => note(e, 622.25, 'sharp')} className="sharp-key"></div>
       </div>
-      <div id='e5' className="key"></div>
-      <div id='f5' className="key">
-        <div id='f5#' className="sharp-key"></div>
+      <div id='e5' onMouseDown={(e) => note(e, 659.26, 'pure')} className="key"></div>
+      <div id='f5' onMouseDown={(e) => note(e, 698.46, 'pure')} className="key">
+        <div id='f5#' onMouseDown={(e) => note(e, 739.99, 'sharp')} className="sharp-key"></div>
       </div>
-      <div id='g5' className="key">
-        <div id='g5#' className="sharp-key"></div>
+      <div id='g5' onMouseDown={(e) => note(e, 783.99, 'pure')} className="key">
+        <div id='g5#' onMouseDown={(e) => note(e, 830.61, 'sharp')} className="sharp-key"></div>
       </div>
-      <div id='a5' className="key">
-        <div id='a5#' className="sharp-key"></div>
+      <div id='a5' onMouseDown={(e) => note(e, 880, 'pure')} className="key">
+        <div id='a5#' onMouseDown={(e) => note(e, 932.33, 'sharp')} className="sharp-key"></div>
       </div>
-      <div id='b5' className="key"></div>
+      <div id='b5' onMouseDown={(e) => note(e, 987.77, 'pure')} className="key"></div>
 
       {/* Octave 6 */}
-      <div id='c6' className="key">
-        <div id='c6#' className="sharp-key"></div>
+      <div id='c6' onMouseDown={(e) => note(e, 1046.5, 'pure')} className="key">
+        <div id='c6#' onMouseDown={(e) => note(e, 1108.7, 'sharp')} className="sharp-key"></div>
       </div>
-      <div id='d6' className="key">
-        <div id='d6#' className="sharp-key"></div>
+      <div id='d6' onMouseDown={(e) => note(e, 1174.7, 'pure')} className="key">
+        <div id='d6#' onMouseDown={(e) => note(e, 1244.5, 'sharp')} className="sharp-key"></div>
       </div>
-      <div id='e6' className="key"></div>
-      <div id='f6' className="key">
-        <div id='f6#' className="sharp-key"></div>
+      <div id='e6' onMouseDown={(e) => note(e, 1318.5, 'pure')} className="key"></div>
+      <div id='f6' onMouseDown={(e) => note(e, 1396.9, 'pure')} className="key">
+        <div id='f6#' onMouseDown={(e) => note(e, 1480, 'sharp')} className="sharp-key"></div>
       </div>
-      <div id='g6' className="key">
-        <div id='g6#' className="sharp-key"></div>
+      <div id='g6' onMouseDown={(e) => note(e, 1568, 'pure')} className="key">
+        <div id='g6#' onMouseDown={(e) => note(e, 1661.2, 'sharp')} className="sharp-key"></div>
       </div>
-      <div id='a6' className="key">
-        <div id='a6#' className="sharp-key"></div>
+      <div id='a6' onMouseDown={(e) => note(e, 1760, 'pure')} className="key">
+        <div id='a6#' onMouseDown={(e) => note(e, 1864.7, 'sharp')} className="sharp-key"></div>
       </div>
-      <div id='b6' className="key border-r-2"></div>
+      <div id='b6' onMouseDown={(e) => note(e, 1975.5, 'pure')} className="key border-r-2"></div>
 
     </div>
   )
