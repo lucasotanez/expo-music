b9e372a6d7f6ad95f3dea4bdfd04c32a8c7a8dc9
Lucas Otanez <lotanez@usc.edu>
2024-01-12 18:16:55 -0800

    add guitar notes in relation to piano

---
+++ app/PianoStrings.tsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from 'react'
+
+export default function PianoStrings() {
+
+  useEffect(() => {
+    document.getElementById("show-guitar-strings")?.addEventListener('change', (e) => {
+      let strings = document.getElementById("strings");
+      if (!strings) return;
+      if ((e.target as HTMLInputElement).checked) {
+        strings.style.display = "flex";
+
+      }
+      else {
+        strings.style.display = "none";
+      }
+    })
+  }, [])
+
+  return (
+    <>
+      <div className="flex flex-col items-center relative w-5/6 h-48">
+        <div className="absolute left-0 flex flex-row items-center">
+          <input type="checkbox" id="show-guitar-strings" value="show-strings" className="mr-3 w-6 h-6"></input>
+          <label>Show Guitar Notes</label>
+        </div>
+        <div id="strings" className="hidden">
+          <div className="left-16/35 bg-orange-400 string">
+            <div className="string-box">E</div><div className="string-box">F</div>
+            <div className="string-box">G</div><div className="string-box">A</div>
+            <div className="string-box">B</div><div className="string-box">C</div>
+            <div className="string-box">D</div><div className="string-box">E</div>
+            <div className="string-box">F</div><div className="string-box">G</div>
+            <div className="string-box">A</div><div className="string-box">B</div>
+            <div className="string-box">C</div><div className="string-box">D</div>
+            <div className="string-box">E</div>
+          </div>
+          <div className="top-8 left-13/35 bg-yellow-300 string">
+            <div className="string-box">B</div><div className="string-box">C</div>
+            <div className="string-box">D</div><div className="string-box">E</div>
+            <div className="string-box">F</div><div className="string-box">G</div>
+            <div className="string-box">A</div><div className="string-box">B</div>
+            <div className="string-box">C</div><div className="string-box">D</div>
+            <div className="string-box">E</div><div className="string-box">F</div>
+            <div className="string-box">G</div><div className="string-box">A</div>
+            <div className="string-box">B</div>
+          </div>
+          <div className="top-16 left-11/35 bg-pink-400 string">
+            <div className="string-box">G</div><div className="string-box">A</div>
+            <div className="string-box">B</div><div className="string-box">C</div>
+            <div className="string-box">D</div><div className="string-box">E</div>
+            <div className="string-box">F</div><div className="string-box">G</div>
+            <div className="string-box">A</div><div className="string-box">B</div>
+            <div className="string-box">C</div><div className="string-box">D</div>
+            <div className="string-box">E</div><div className="string-box">F</div>
+            <div className="string-box">G</div>
+          </div>
+          <div className="top-24 left-8/35 bg-green-400 string">
+            <div className="string-box">D</div>
+            <div className="string-box">E</div><div className="string-box">F</div>
+            <div className="string-box">G</div><div className="string-box">A</div>
+            <div className="string-box">B</div><div className="string-box">C</div>
+            <div className="string-box">D</div><div className="string-box">E</div>
+            <div className="string-box">F</div><div className="string-box">G</div>
+            <div className="string-box">A</div><div className="string-box">B</div>
+            <div className="string-box">C</div><div className="string-box">D</div>
+          </div>
+          <div className="top-30 left-5/35 bg-blue-400 string">
+            <div className="string-box">A</div><div className="string-box">B</div>
+            <div className="string-box">C</div><div className="string-box">D</div>
+            <div className="string-box">E</div><div className="string-box">F</div>
+            <div className="string-box">G</div><div className="string-box">A</div>
+            <div className="string-box">B</div><div className="string-box">C</div>
+            <div className="string-box">D</div><div className="string-box">E</div>
+            <div className="string-box">F</div><div className="string-box">G</div>
+            <div className="string-box">A</div>
+          </div>
+          <div className="top-38 left-2/35 bg-orange-400 string">
+            <div className="string-box">E</div><div className="string-box">F</div>
+            <div className="string-box">G</div><div className="string-box">A</div>
+            <div className="string-box">B</div><div className="string-box">C</div>
+            <div className="string-box">D</div><div className="string-box">E</div>
+            <div className="string-box">F</div><div className="string-box">G</div>
+            <div className="string-box">A</div><div className="string-box">B</div>
+            <div className="string-box">C</div><div className="string-box">D</div>
+            <div className="string-box">E</div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,92 +3,9 @@ import Image from 'next/image'
 import { useEffect } from 'react'
 import play from './audio'
 import Piano from './Piano'
+import PianoStrings from './PianoStrings'
 
 export default function Home() {
-
-  //var comp = [
-  //  [261, 200],
-  //  [294, 200],
-  //  [330, 200],
-  //  [349, 200],
-  //  [392, 200],
-  //  [440, 200],
-  //  [494, 200],
-  //  [523, 400], // C5
-  //]
-
-  var middle = {
-    C: 261,
-    D: 294,
-    E: 330,
-    F: 349,
-    G: 392,
-    A: 440,
-    B: 494
-  }
-
-  let beat = 300
-
-  var ohSus = [
-    [middle.C, beat],
-    [middle.D, beat],
-    [middle.E, beat],
-    [middle.G, beat],
-    [middle.G, beat],
-    [middle.A, beat],
-    [middle.G, beat],
-    [middle.E, beat],
-    [middle.C, beat],
-    [middle.D, beat],
-    // newline
-    [middle.E, beat],
-    [middle.E, beat],
-    [middle.D, beat],
-    [middle.C, beat],
-    [middle.D, beat * 2],
-    [middle.C, beat],
-    [middle.D, beat],
-    [middle.E, beat],
-    [middle.G, beat],
-    [middle.G, beat],
-    [middle.A, beat],
-    //newline
-    [middle.G, beat],
-    [middle.E, beat],
-    [middle.C, beat],
-    [middle.D, beat],
-    [middle.E, beat],
-    [middle.E, beat],
-    [middle.D, beat],
-    [middle.D, beat],
-    [middle.C, beat * 2],
-
-  ]
-
-  var wish = [
-
-    [middle.G, beat],
-    [middle.G, beat],
-    [middle.E, beat],
-    [middle.D, beat * 3],
-    [0, beat / 2],
-
-    [middle.G, beat / 2],
-    [middle.G, beat / 2],
-    [middle.G, beat / 2],
-    [0, beat / 2],
-
-    [middle.G, beat / 2],
-    [middle.G, beat / 2],
-    [0, beat / 2],
-
-    [196, beat],
-    [247, beat],
-    [middle.D, beat],
-    [middle.E, beat * 3],
-
-
-  ]
 
   useEffect(
     () => {
@@ -97,7 +14,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <div className="w-5/6 bg-gray-100 rounded-lg shadow-sm flex flex-col items-center">
+      <div className="w-5/6 bg-gray-100 rounded-lg shadow-sm flex flex-col items-center min-w-44">
         <div className="flex flex-col items-start mb-4 mt-8 w-5/6">
           <input type='text' placeholder='title' className="duration-200 px-2 
             py-1 my-4 text-xl bg-transparent hover:bg-gray-300 outline-none rounded-lg"></input>
@@ -105,7 +22,6 @@ export default function Home() {
         <div id="controls" className="flex flex-row items-center mb-10">
           <button className="bg-green-500 text-white py-1 px-3 rounded-lg font-bold"
             onClick={() => {
-              play(wish);
             }}>Play</button>
         </div>
         <div id="sources" className="flex flex-col w-5/6 m-auto mb-12">
@@ -119,6 +35,16 @@ export default function Home() {
       </div>
 
       <Piano />
+
+      <PianoStrings />
+
+
+
+      <div>
+        <Image src="/guitar_piano.png" alt="guitar-piano-comparison" height='500' width='700'></Image>
+
+
+      </div>
     </main>
   )
 }
