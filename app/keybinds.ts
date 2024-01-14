ef77f0daae1e1fe09e78b0a4a4a313e3151784df
Lucas Otanez <lotanez@usc.edu>
2024-01-13 17:04:16 -0800

    add key mapping and note to freq mapping

---
+++ app/keybinds.ts
@@ -0,0 +1,30 @@
+export var binds = new Map<string, string>([
+  ['KeyA', 'c4'],
+  ['KeyS', 'c4#'],
+  ['KeyD', 'd4'],
+  ['KeyF', 'd4#'],
+  ['KeyG', 'e4'],
+  ['KeyH', 'f4'],
+  ['KeyJ', 'f4#'],
+  ['KeyK', 'g4'],
+  ['KeyL', 'g4#'],
+  ['Semicolon', 'a4'],
+  ['Quote', 'a4#'],
+  ['Enter', 'b4'],
+])
+
+
+export var notes = new Map<string, number>([
+  ['c4', 261.6],
+  ['c4#', 277.1],
+  ['d4', 293.7],
+  ['d4#', 311.1],
+  ['e4', 329.6],
+  ['f4', 349.2],
+  ['f4#', 369.99],
+  ['g4', 392],
+  ['g4#', 415.3],
+  ['a4', 440],
+  ['a4#', 466.16],
+  ['b4', 493.88],
+])
